189557838424c49dfdcba24c2ba9cb2a391f3abc
SEOYOUNGSLAPTOP\Info10 <cso151515@gmail.com>
2024-05-20 13:58:27 +0900

    1주차 문제 해결

--- week1.java
+++ week1.java
@@ -1,33 +1,28 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
 public class week1 {
+    protected static int scanPerson(Scanner scanner) {
+        System.out.println("참여할 사람은 몇 명 인가요?");
+        return Integer.parseInt(scanner.nextLine());
+    }
+    protected static int scanLadder(Scanner scanner) {
+        System.out.println("최대 사다리 높이는 몇 개 인가요?");
+        return Integer.parseInt(scanner.nextLine());
+    }
     public static void main(String[] args) {
-
-        //scanner 선언
         Scanner scanner = new Scanner(System.in);
-        //random 선언
         Random  random = new Random();
 
-        System.out.println("참여할 사람은 몇 명인가요?");
-        String scan1 = scanner.nextLine();
-        int person = Integer.parseInt(scan1);
+        int person = scanPerson(scanner);
+        int ladder = scanLadder(scanner);
 
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        String scan2 = scanner.nextLine();
-        int maxLadder = Integer.parseInt(scan2);
-
-        for (int i=0; i<maxLadder; i++){
+        for (int i=0; i<ladder; i++){
             for (int j=0; j<person; j++){
                 System.out.print("|");
                 if (j < person - 1) {
                     int ladderLine = random.nextInt(2);
-
-                    if (ladderLine == 0) {
-                        System.out.print("-");
-                    } else {
-                        System.out.print(" ");
-                    }
+                    System.out.print(ladderLine == 0 ? "-" : " ");
                 }
             }
             System.out.println();
